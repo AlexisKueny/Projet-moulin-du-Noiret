167e53a9a13228de2dbd8cd324cf826c1bf6d1de
Alex <alexis.kueny@outlook.com>
2025-11-10 16:45:06 +0100

    import timeline

--- moulin-app/src/main.tsx
+++ moulin-app/src/main.tsx
@@ -5,6 +5,7 @@ import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import Home from './pages/Home.tsx'
 import App from './App.tsx'
 import { blueGrey, grey } from '@mui/material/colors'
+import History from './pages/History.tsx'
 
 const theme = createTheme({
   palette: {
@@ -26,7 +27,7 @@ createRoot(document.getElementById('root')!).render(
           <Routes>
             <Route path="/" element={<App />}>
               <Route index element={<Home />} />
-              <Route path="historique" element={<h1>Historique</h1>} />
+              <Route path="historique" element={<History/>} />
               <Route path="projet" element={<h1>Projet</h1>} />
               <Route path="travaux" element={<h1>Travaux</h1>} />
               <Route path="*" element={<h1>Page non trouvée</h1>} />
