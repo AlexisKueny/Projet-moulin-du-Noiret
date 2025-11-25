2606f012233ad6798bc500ae6b0139f63e4859f0
Alex <alexis.kueny@outlook.com>
2025-11-25 19:49:19 +0100

    Hash router

--- moulin-app/src/main.tsx
+++ moulin-app/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import Home from './pages/Home.tsx'
 import App from './App.tsx'
@@ -23,7 +23,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssBaseline >
       <ThemeProvider theme={theme}>
-        <BrowserRouter basename="/moulindunoiret/">
+        <HashRouter basename="/moulindunoiret/">
           <Routes>
             <Route path="/" element={<App />}>
               <Route index element={<Home />} />
@@ -33,7 +33,7 @@ createRoot(document.getElementById('root')!).render(
               <Route path="*" element={<h1>Page non trouvée</h1>} />
             </Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </CssBaseline>
   </StrictMode>
